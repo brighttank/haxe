fa2876638e36da2406fb3e311c8a118cba53372e
GitHub Action <action@github.com>
2022-04-06 07:26:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/brighttank/haxe/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/brighttank/haxe/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.brighttank.haxe.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.brighttank.haxe.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/test/kotlin/com/github/brighttank/haxe/MyPluginTest.kt
+++ src/test/kotlin/com/github/brighttank/haxe/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.brighttank.haxe
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
